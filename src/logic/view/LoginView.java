aad5faeec1bc636af4092a009c3c1cd1f3a19349
Michele <40394496+MicheleSalvatori@users.noreply.github.com>
2020-12-15 13:16:39 +0100

    Css per Textfield e button

--- src/logic/view/LoginView.java
+++ src/logic/view/LoginView.java
@@ -9,6 +9,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import logic.bean.UserBean;
+import logic.controller.LoginController;
 
 public class LoginView implements Initializable{
 
@@ -18,19 +20,35 @@ public class LoginView implements Initializable{
     private TextField textUsername;
     @FXML
     private PasswordField textPassword;
+    
+    private UserBean userBean;
+    private LoginController loginController;
+    
 
     @FXML
     void loginUser(ActionEvent event) {
+    	boolean logged;
     	String user = textUsername.getText();
     	String password = textPassword.getText();
     	
+    	userBean = UserBean.getUserBeanInstance(user);
+    	userBean.setUsbPassword(password);
     	
+    	loginController = new LoginController();
+    	logged = loginController.login(userBean);
+    	
+    	if (!logged) {
+    		UserBean.setInstance(null);
+    		// show Alert o gestione exception?
+    	}
+    	
+    	// load Homepage
     }
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		textUsername.requestFocus();
+		btnLogin.setDisable(false);
 	}
 
 }
