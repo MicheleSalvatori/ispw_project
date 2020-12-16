bb156ec6cd7029b0bcb614d78941c77ab57063ea
Michele <40394496+MicheleSalvatori@users.noreply.github.com>
2020-12-16 20:28:21 +0100

    PageLoader Merged

--- src/logic/utilities/PageLoader.java
+++ src/logic/utilities/PageLoader.java
@@ -6,16 +6,23 @@ import java.net.URL;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import logic.view.NavBar;
+import logic.view.StatusBar;
 
 public class PageLoader {
 	private Page page;
-
+	private Stage primaryStage;
+	private HBox mainLayoutHBox;
+	private Scene scene;
+	
 	public PageLoader(Page page, ActionEvent event) throws IOException {
 		this.page = page;
 		if (page.getStageTitle() == "App - Signup" || page.getStageTitle() == "App - Login") {
@@ -28,14 +35,46 @@ public class PageLoader {
 
 	private void loadPage(ActionEvent ae) throws IOException {
 		Node source = (Node) ae.getSource();
+		primaryStage = (Stage) source.getScene().getWindow();
+		URL url = new File(page.getRes()).toURI().toURL();
+		FXMLLoader loader = new FXMLLoader(url);
+		
+		configPage(loader.load());
+
+	}
+	
+	private NavBar configNavBar() throws IOException {
+		return NavBar.getInstance();
+	}
+	
+	private HBox configStatusBar() throws IOException {
+		HBox statusBarHBox = new HBox(StatusBar.getInstance());
+		statusBarHBox.setAlignment(Pos.TOP_RIGHT);
+		HBox.setMargin(StatusBar.getInstance(), new Insets(10, 10, 10, 0));
+		return statusBarHBox;
+	}
+	
+	private void configPage(Parent pageView) throws IOException {
+		
+		VBox vBox = new VBox();
+		vBox.getChildren().addAll(configStatusBar(), pageView);
+		mainLayoutHBox = new HBox(configNavBar(), vBox);						
+		HBox.setMargin(NavBar.getInstance(), new Insets(30.0, 0,10.0,10.0));
+		
+		scene = new Scene(mainLayoutHBox);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle(page.getStageTitle());
+		primaryStage.show();
+	}
+
+private void loadPageNoNavBar(ActionEvent ae) throws IOException {
+		Node source = (Node) ae.getSource();
 		Stage stage = (Stage) source.getScene().getWindow();
 		URL url = new File(page.getRes()).toURI().toURL();
 		FXMLLoader loader = new FXMLLoader(url);
 		Parent parent = loader.load();
-		NavBar navBar = NavBar.getInstance();
-		HBox hBox = new HBox(navBar, parent);			//TODO aggiungere status bar
 
-		Scene scene = new Scene(hBox);
+		Scene scene = new Scene(parent);
 		stage.setScene(scene);
 		stage.setTitle(page.getStageTitle());
 		stage.show();
