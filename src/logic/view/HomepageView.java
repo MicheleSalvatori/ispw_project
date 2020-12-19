27fcc56f1a27b24d74541f2bdcc08766a2b3be34
Gabriele <57992276+gabrielequatrana@users.noreply.github.com>
2020-12-19 21:41:05 +0100

    [68] Added some features to Homepage

--- src/logic/view/HomepageView.java
+++ src/logic/view/HomepageView.java
@@ -1,14 +1,43 @@
 package logic.view;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class HomepageView implements Initializable {
+	
+	@FXML
+	private ScrollPane scroll;
 
-    
+	private VBox box;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		box = new VBox();
+		for (int i=0; i<10; i++) {
+			LessonCard lessonCard;
+			try {
+				lessonCard = new LessonCard(i+"",i+"",i+"");
+				box.getChildren().add(lessonCard);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		box.setSpacing(20);
+		
+		
+		scroll.setContent(box);
 	}
 
 }
---
+++ src/logic/view/LessonCardView.java
@@ -0,0 +1,26 @@
+package logic.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class LessonCardView implements Initializable {
+	
+	@FXML
+	Label labelLesson, labelClass, labelTime;
+	
+	public void setLabel(String lesson, String classroom, String time) {
+		labelLesson.setText(lesson);
+		labelClass.setText(classroom);
+		labelTime.setText(time);
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
