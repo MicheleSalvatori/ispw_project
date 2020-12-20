ec69edd7d23835ba72f89cb98a3aeb9051421e15
Gabriele <57992276+gabrielequatrana@users.noreply.github.com>
2020-12-20 18:21:39 +0100

    [71] Added WeatherCard.fxml

--- src/logic/view/HomepageView.java
+++ src/logic/view/HomepageView.java
@@ -9,18 +9,27 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import logic.Session;
+import logic.model.User;
 
 public class HomepageView implements Initializable {
 	
 	@FXML
 	private ScrollPane scroll;
+	@FXML
+	private Label labelUsername;
 
 	private VBox box;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		User user = Session.getSession().getUserLogged();
+		
+		labelUsername.setText("Hello " + user.getName() + "!");
 		
 		box = new VBox();
 		for (int i=0; i<10; i++) {
@@ -34,8 +43,7 @@ public class HomepageView implements Initializable {
 			}
 		}
 		
-		box.setSpacing(20);
-		
+		box.setSpacing(20);	
 		
 		scroll.setContent(box);
 	}
