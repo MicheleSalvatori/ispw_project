724ddf45576097fb42b68d0fab984fb8e8542e28
Michele <40394496+MicheleSalvatori@users.noreply.github.com>
2021-01-08 17:16:24 +0100

    [251] Added attrs. to Classroom

--- src/logic/model/Classroom.java
+++ src/logic/model/Classroom.java
@@ -1,11 +1,18 @@
 package logic.model;
 
 public class Classroom {
-
+	private int seatRow;
+	private int seatColumn;
 	private String name;
-	
+
 	public Classroom(String name) {
 		this.name = name;
+	}
+
+	public Classroom(String name, int seatRow, int seatColumn) {
+		this.name = name;
+		this.seatColumn = seatColumn;
+		this.seatRow = seatRow;
 	}
 
 	public String getName() {
@@ -15,4 +22,21 @@ public class Classroom {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getSeatRow() {
+		return seatRow;
+	}
+
+	public void setSeatRow(int seatRow) {
+		this.seatRow = seatRow;
+	}
+
+	public int getSeatColumn() {
+		return seatColumn;
+	}
+
+	public void setSeatColumn(int seatColumn) {
+		this.seatColumn = seatColumn;
+	}
+
 }
