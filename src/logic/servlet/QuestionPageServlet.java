df93f51abc8f0f5e24cb08c56555da738790e454
Michele <40394496+MicheleSalvatori@users.noreply.github.com>
2021-01-22 19:08:57 +0100

    [311] Added QuestionPage  with Assignment (WEB)

---
+++ src/logic/servlet/QuestionPageServlet.java
@@ -0,0 +1,59 @@
+package logic.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import logic.bean.AssignmentBean;
+import logic.bean.QuestionBean;
+import logic.bean.UserBean;
+import logic.controller.AddAssignmentController;
+import logic.controller.AllQuestionController;
+import logic.exceptions.RecordNotFoundException;
+
+@WebServlet("/QuestionPageServlet")
+public class QuestionPageServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		AllQuestionController controller = new AllQuestionController();
+		List<QuestionBean> questions = null;
+		try {
+			questions = controller.getAllQuestions( (UserBean) session.getAttribute("loggedUser"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		request.setAttribute("listOfQuestion", questions);
+		
+		AddAssignmentController assignmentController = new AddAssignmentController();
+		List<AssignmentBean> assignments = null;
+		try {
+			assignments = assignmentController.getAssignments((UserBean) session.getAttribute("loggedUser"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (RecordNotFoundException e) {
+			e.printStackTrace();
+		}
+		request.setAttribute("listOfAssignment", assignments);
+		request.getRequestDispatcher("/WEB-INF/QuestionPage.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+	
+	
+}
